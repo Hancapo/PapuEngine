c92bcd5042deeac5272eeb7e0ac333ba8d386f96
MrVicho13 <vicho0202@gmail.com>
2025-04-15 23:05:29 -0400

    refactor: move texture loading to texture.

--- PapuEngine/Texture.cs
+++ PapuEngine/Texture.cs
@@ -1,4 +1,8 @@
-﻿using OpenTK.Graphics.OpenGL4;
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using OpenTK.Graphics.OpenGL4;
+using PixelFormat = OpenTK.Graphics.OpenGL4.PixelFormat;
+using PixelFormatSystem = System.Drawing.Imaging.PixelFormat;
 
 namespace PapuEngine;
 
@@ -16,7 +20,43 @@ public class Texture(
 
     public void Load()
     {
-        Id = ImageHelper.LoadImage(_path, _repeat, _min, _mag);
+        Id = LoadImage(_path, _repeat, _min, _mag);
+    }
+    
+    private int LoadImage(string? path, bool repeat, 
+        TextureMinFilter textureMinFilter = 
+            TextureMinFilter.Linear, 
+        TextureMagFilter textureMagFilter = TextureMagFilter.Linear)
+    {
+        var textureId = GL.GenTexture();
+        GL.BindTexture(TextureTarget.Texture2D, textureId);
+
+        using (var image = new Bitmap(path))
+        {
+            image.RotateFlip(RotateFlipType.RotateNoneFlipY);
+
+            var data = image.LockBits(
+                new Rectangle(0, 0, image.Width, image.Height),
+                ImageLockMode.ReadOnly,
+                PixelFormatSystem.Format32bppArgb);
+            
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, image.Width, image.Height, 
+                0, PixelFormat.Bgra,PixelType.UnsignedByte, data.Scan0);
+            
+            image.UnlockBits(data);
+        }
+        
+        TextureWrapMode twm = repeat ? TextureWrapMode.Repeat : TextureWrapMode.ClampToEdge;
+        TextureMinFilter tminf = TextureMinFilter.Linear == textureMinFilter ? TextureMinFilter.Linear : TextureMinFilter.Nearest;
+        TextureMagFilter tmf = TextureMagFilter.Linear == textureMagFilter ? TextureMagFilter.Linear : TextureMagFilter.Nearest;
+        
+        GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)tminf);
+        GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)tmf);
+        GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)twm);
+        GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)twm);
+
+        return textureId;
+
     }
 
     public void Bind(TextureUnit unit = TextureUnit.Texture0)
